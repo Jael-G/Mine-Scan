25fc8492627889e11c5dc0a0cbf6300a9ff93d10
Jael Gonzalez <jael.gonzalez1@upr.edu>
2024-10-24 06:28:10 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,136 @@
+![Mine Scan banner image](/images/mine_scan_banner.png)
+
+# Mine Scan - Multi-Threaded Minecraft Server Scanner
+
+A powerful and advanced multi-threaded Python Minecraft Server scanner. Retrieves server details such as version, player count, and latency. Supports scanning from a file containing *IP* ranges (specified by upper and lower bounds), individual *IP* addresses, or using CIDR notation to define ranges. Allows customization of threads, timeout, port and output file. 
+
+## Table of Contents
+- [Usage](#usage)
+- [How it Works](#how-it-works)
+- [Installation](#installation)
+- [Using IP Ranges File](#using-ip-ranges-file)
+- [To-Do](#to-do)
+- [License](#license)
+- [Disclaimer](#disclaimer)
+
+# Usage
+
+To run the scan simply execute the script with `python3 minescan.py` and provide the necessary arguments (ensure you have installed the [requirements](#installation)). 
+
+```
+usage: tool [-h] [--threads threads] [--timeout seconds] (--filepath filepath | --ip IP) [--port port] [--output output]
+
+options:
+  -h, --help                        show this help message and exit
+  --threads threads, -th threads    Threads for Pool (default=64)
+  --timeout seconds, -ti seconds    Server status timeout (default=2s)
+  --filepath filepath, -f filepath  IP ranges file path
+  --ip IP, -i IP                    Single IP or CIDR to scan
+  --port port, -p port              Minecraft server port (default=25565)
+  --output output, -o output        Output filepath (default=results.json)
+
+Verify local laws before scanning and ensure you have permission to scan the network. Use at your own risk.
+```
+
+The speed of the scan is primarily influenced by two factors: threading and timeout settings. Increasing the number of threads enables more simultaneous scans, while reducing the timeout decreases the wait time for responses.
+
+Timeout greatly impacts the speed since each thread must wait for the full timeout duration when there’s no server response before it can move on to the next IP address.
+
+However, setting the timeout too low can lead to false timeouts, potentially missing responses from IPs that do have servers. Based on testing, a 2-second timeout results in minimal false timeouts. However, this can vary depending on factors such as connectivity and distance from the *IP* being scanned.
+
+The scan always outputs a JSON file containing the successfully scanned servers. This file is updated at the end of each range scan, so it's often better to divide large *IP* ranges into smaller segments for more efficient processing.
+
+#### Example
+
+[![asciicast](https://asciinema.org/a/683526.svg)](https://asciinema.org/a/683526)
+
+**Note: Some emojis might not render properly in asciinema depending on your system**
+
+# How It Works
+
+***Mine Scan*** uses the `mcstatus` Python library to check if a server is running in a specified *IP* and port. If there's a response then certain server data is stored in a JSON file. This data includes:
+
+- Server Version
+- Current players and maximum players
+- Latency
+
+The program uses `tqdm` to display the progress per-range and `termcolor` to stylize everything. 
+
+# Installation
+To get started download the repository, install the necessary dependencies, and run the script:
+
+1. Clone the repository:
+
+```
+git clone https://github.com/Jael-G/Mine-Scan
+```
+
+2. Navigate to the project directory:
+
+```
+cd Mine-Scan
+```
+
+3. Install the required packages:
+
+```
+python3 -m pip install -r requirements.txt
+```
+
+# Using IP Ranges File
+
+One of the standout features of ***Mine Scan*** is its ability to manage large files with *IP* ranges. Each range specifies a lower and upper *IP* address for scanning, and the program generates all possible IPs in between.
+
+This functionality is particularly useful when utilizing [databases](https://cable.ayra.ch/ip/) that provide lists of all public IPs for a specific country (or multiple countries) in the form of upper and lower *IP* ranges. The file should include only *IP* ranges, formatted as follows, with one range per line:
+
+```
+lower_ip upper_ip
+```
+
+In the [example](#example) the ranges file was:
+
+```
+8.8.8.1 8.8.8.254
+8.8.4.1 8.8.4.254
+```
+
+In testing when using a file that contained 478 ranges (around 3.6 million total public IPs) it took less than an hour to complete the scan (with an extremely powerful CPU server and thousands of threads).
+
+# To-Do
+
+1. Implement option to allow single IPs and CIDR notation on a file of IP ranges (currently only allows lower and upper range separated by a space)
+
+2. Fix JSON formatting. Currently, it lacks the brackets (`[]`) at the start and end of the file in order to be a properly formatted JSON. 
+
+
+# License
+
+```
+MIT License
+
+Copyright (c) 2024 Jael Gonzalez
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+```
+
+# Disclaimer
+
+**Usage Responsibility**: This tool is intended for educational and research purposes only. Users are **strongly** advised to verify local laws and obtain permission before scanning any networks or IP addresses. **Unauthorized scanning can violate privacy rights and laws**.
+
+**No Malicious Use**: The creator does not **endorse or take responsibility for any malicious or harmful activities** that may result from the use of this tool. Users must use this software ethically and responsibly, respecting the rights and privacy of others.
---
+++ minescan.py
@@ -0,0 +1,266 @@
+from mcstatus import JavaServer, status_response
+from multiprocessing.dummy import Pool as ThreadPool
+from tqdm import tqdm
+from termcolor import colored
+from os import get_terminal_size
+
+import ipaddress
+import time
+import json
+import argparse
+
+
+# Prints banner at start
+def display_start_banner(args, ranges_amount=1):
+    banner_string = """\n███╗   ███╗██╗███╗   ██╗███████╗    ███████╗ ██████╗ █████╗ ███╗   ██╗
+████╗ ████║██║████╗  ██║██╔════╝    ██╔════╝██╔════╝██╔══██╗████╗  ██║
+██╔████╔██║██║██╔██╗ ██║█████╗      ███████╗██║     ███████║██╔██╗ ██║
+██║╚██╔╝██║██║██║╚██╗██║██╔══╝      ╚════██║██║     ██╔══██║██║╚██╗██║
+██║ ╚═╝ ██║██║██║ ╚████║███████╗    ███████║╚██████╗██║  ██║██║ ╚████║
+╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚══════╝    ╚══════╝ ╚═════╝╚═╝  ╚═╝╚═╝  ╚═══╝
+                                                                      
+         ⛏️  Advanced Minecraft Multi-Threaded Server Scanner
+                          
+ """
+
+    stats_string = f"📊 Stats:\n"
+
+    if args.ip:
+        stats_string += f"\t🎯 Target: {args.ip}\n"
+    else:
+        stats_string += f"\t🎯 Total Ranges: {ranges_amount}\n"
+
+    stats_string += f"\t🧵 Threads: {thread_num}\n\t⚓ Port: {args.port}\n\t⌛ Timeout: {args.timeout}\n"
+
+    print(colored(banner_string, "light_red", attrs=["bold"]))
+    print(colored(stats_string, "light_yellow"))
+
+
+def parse_arguments() -> argparse.Namespace:
+    """Parses command line arguments for the scanner
+
+    Returns:
+        argparse.Namespace: Parsed arguments containing threads,
+        IP, or filepath.
+    """
+    arg_parser = argparse.ArgumentParser(
+        prog="tool",
+        formatter_class=lambda prog: argparse.HelpFormatter(
+            prog, max_help_position=get_terminal_size().columns
+        ),
+        description=(
+            "Mine Scan - "
+            "A powerful advanced multi-threaded Python Minecraft scanner. "
+            "Retrieves server details such as version, player count, and latency. "
+            "Supports scanning from a file containing IP ranges (specified by upper and lower bounds), "
+            "individual IP addresses, or using CIDR notation to define ranges."
+        ),
+        epilog="Verify local laws before scanning and ensure you have permission to scan the network. Use at your own risk. ",
+    )
+    arg_parser.add_argument(
+        "--threads",
+        "-th",
+        help="Threads for Pool (default=64)",
+        default=64,
+        metavar="threads",
+    )
+
+    arg_parser.add_argument(
+        "--timeout",
+        "-ti",
+        help="Server status timeout (default=2s)",
+        default=2,
+        metavar="seconds",
+    )
+
+    target_group = arg_parser.add_mutually_exclusive_group(required=True)
+
+    target_group.add_argument(
+        "--filepath", "-f", help="IP ranges file path", metavar="filepath"
+    )
+
+    target_group.add_argument(
+        "--ip", "-i", help="Single IP or CIDR to scan", metavar="IP"
+    )
+
+    arg_parser.add_argument(
+        "--port",
+        "-p",
+        help="Minecraft server port (default=25565)",
+        default=25565,
+        metavar="port",
+    )
+
+    arg_parser.add_argument(
+        "--output",
+        "-o",
+        help="Output filepath (default=results.json)",
+        default="results.json",
+        metavar="output",
+    )
+
+    return arg_parser.parse_args()
+
+
+def check_server(ip: str, port: int, pbar: tqdm, timeout: float) -> dict:
+    """Looks up a server status of a given IP and port
+
+    Args:
+        ip (str): Minecraft server ip
+        port (int): Defaults to 25565.
+        pbar (tqdm): TQDM Bar to update
+        timeout (float): Server scan timeout
+
+    Returns:
+        dict: Returns dict of server data, empty if no server found
+    """
+
+    server_data = {}
+    try:
+        server = JavaServer.lookup(f"{ip}:{port}", timeout)
+        status: status_response.JavaStatusResponse = server.status()
+
+        server_data[ip] = {}
+        server_data[ip]["version"] = status.version.name
+        server_data[ip]["latency"] = f"{status.latency:.2f}"
+        server_data[ip]["players"] = f"{status.players.online}/{status.players.max}"
+
+    except:
+        pass
+
+    pbar.update(1)
+    return server_data
+
+
+def generate_target_ips(ip_lower: str, ip_upper: str = None) -> list[str]:
+    """Generates all possible IPs
+
+    Args:
+        ip_lower (str): The starting IP address or CIDR block.
+        ip_upper (str, optional): The ending IP address (for range-based generation).
+
+    Returns:
+        list[str]: List target IPs
+    """
+
+    if ip_upper:  # Generate using range
+        lower = int(ipaddress.ip_address(ip_lower))
+        upper = int(ipaddress.ip_address(ip_upper))
+        ips = []
+
+        for i in range(lower, upper + 1):
+            ips.append(str(ipaddress.ip_address(i)))
+        return ips
+
+    if "/" in ip_lower:  # Generate using CIDR
+        network = ipaddress.ip_network(ip_lower, strict=False)
+        ips = [str(ip) for ip in network.hosts()]
+        return ips
+
+    return [ip_lower]  # Return single IP
+
+
+def parse_results(results: list[dict], run_time: float) -> None:
+    """Parses and demonstrates the scan results
+
+    Args:
+        results (list[dict]): List of all server data dicts
+        run_time (float): Time to complete scan
+    """
+
+    hits: list[dict] = [server_data for server_data in results if server_data != {}]
+    misses = len(results) - len(hits)
+    total = len(results)
+
+    print(
+        colored(
+            f"✅ Done → {len(hits)}|{misses}|{total} [{run_time:.2f}s]\n",
+            "light_green",
+        )
+    )
+
+    with open(output_filepath, "a") as json_file:
+        for server_data in hits:
+            json_file.write(f"{json.dumps(server_data)}\n")
+
+
+def count_lines(filepath: str) -> int:
+    """Count the lines in a filepath
+
+    Args:
+        filepath (str): IP ranges file
+
+    Returns:
+        int: Amount of lines in the file
+    """    
+    with open(filepath, "r") as ranges_file:
+        return sum(1 for line in ranges_file)
+
+
+def execute_thread_pool(
+    target_ips: list[str], port: int, thread_num: int, timeout: float
+) -> None:
+    """Executes thread pool to scan a given IP range
+
+    Args:
+        target_ips (list[str]): IPs to scan
+        port (int): Minecraft server port
+        thread_num (int): Amount of threads for Pool
+        timeout (float): Server scan timeout
+    """
+    pool = ThreadPool(thread_num)
+
+    start_time = time.time()
+
+    print(colored(f"⚙️  Selected {len(target_ips)} IPs", "light_yellow"))
+
+    with tqdm(
+        total=len(target_ips),
+        bar_format=colored("{l_bar}{bar}| {n_fmt}/{total_fmt}  ", "light_yellow"),
+        unit="IP",
+        unit_scale=True,
+    ) as pbar:
+
+        try:
+            results: list[dict] = pool.map(
+                lambda ip: check_server(ip, port, pbar, timeout), target_ips
+            )
+        except Exception as e:
+            print(colored(f"Error occurred: {e}", "red"))
+        finally:
+            run_time = time.time() - start_time
+            pool.close()
+            pool.join()
+
+    parse_results(results, run_time)
+
+
+if __name__ == "__main__":
+    args: argparse.Namespace = parse_arguments()
+    thread_num = int(args.threads)
+    timeout = float(args.timeout)
+    port = int(args.port)
+    output_filepath = args.output
+
+
+    if args.ip: # IP or CIDR
+        display_start_banner(args)
+        print(colored(f"🔍 Scanning: {args.ip}", "light_cyan"))
+
+        target_ips = generate_target_ips(args.ip)
+        execute_thread_pool(target_ips, port, thread_num, timeout)
+
+    else: # IP range File
+        ranges_amout = count_lines(args.filepath)
+        display_start_banner(args, ranges_amout)
+
+        with open(args.filepath, "r") as ranges_file:
+            for line in ranges_file:
+                ip_range = tuple(line.split()[0:2])
+                print(colored(f"🔍 Scanning: {ip_range[0]} - {ip_range[1]}", "light_cyan"))
+
+                try:
+                    target_ips: list[str] = generate_target_ips(ip_range[0], ip_range[1])
+                    execute_thread_pool(target_ips, port, thread_num, timeout)
+                except Exception as e:
+                    print(colored(f"Error scanning range: {e}\n", 'light_red'))
